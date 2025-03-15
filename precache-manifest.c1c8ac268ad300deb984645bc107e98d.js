0c8c89aed42b608eadb67534edfeb3298dc52650
Abhishek <abhi1kush@gmail.com>
2025-03-16 03:24:25 +0530

    Updates

---
+++ precache-manifest.c1c8ac268ad300deb984645bc107e98d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c84e6bcb247ea724ed558343f8f866",
+    "url": "/chess-frontend/index.html"
+  },
+  {
+    "revision": "d110b2aced1f6a0275dd",
+    "url": "/chess-frontend/static/css/main.c1ebc90f.chunk.css"
+  },
+  {
+    "revision": "fbc788c959346f22e55d",
+    "url": "/chess-frontend/static/js/2.2d2ef04f.chunk.js"
+  },
+  {
+    "revision": "10a8240a3fde81e60c91c4f31cb29ef0",
+    "url": "/chess-frontend/static/js/2.2d2ef04f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cda761ab1c3ac74bf487",
+    "url": "/chess-frontend/static/js/3.6b8bb3ff.chunk.js"
+  },
+  {
+    "revision": "d110b2aced1f6a0275dd",
+    "url": "/chess-frontend/static/js/main.64974f9f.chunk.js"
+  },
+  {
+    "revision": "4517d90c89077c60ea9a",
+    "url": "/chess-frontend/static/js/runtime-main.eeacb1ca.js"
+  },
+  {
+    "revision": "16a09c1c4a221742bfce4a77d468bf5f",
+    "url": "/chess-frontend/static/media/capture.16a09c1c.mp3"
+  },
+  {
+    "revision": "86644655480b78ea94e323841b33fe5b",
+    "url": "/chess-frontend/static/media/move.86644655.mp3"
+  },
+  {
+    "revision": "9ee53a8db449017e972f785b3211a682",
+    "url": "/chess-frontend/static/media/wrong.9ee53a8d.mp3"
+  }
+]);
