da712185b8deb3ddc8a512a87575830a29f320d7
abhishek <abhi1kush@gmail.com>
2025-04-23 06:14:04 +0530

    adding fen_validation

--- src/services/fen/fen_validation.js
+++ src/services/fen/fen_validation.js
@@ -1,9 +1,38 @@
-// const IsValidFen = (fen) => {
-//     const splittedFen = fen.split(" ");
-//     const ranks = splittedFen[0].split("/");
-//     const lastRank = ranks[0];
-//     const fistRank = ranks[7];
-//     for (let i = 0; i < 8; i++) {
-//         if (i < )
-//     }
-// }
+const IsValidFen = (fen) => {
+    const splittedFen = fen.split(" ");
+    const ranks = splittedFen[0].split("/");
+    const lastRank = ranks[0];
+    const firstRank = ranks[7];
+    let blackKingCount = 0;
+    let whiteKingCount = 0;
+
+    for (let r = 0; r < 8; r++) {
+        const currentRank = ranks[r];
+        for (let i = 0; i < currentRank.length; i++) {
+            switch(currentRank[i]) {
+                case 'k':
+                    blackKingCount += 1;
+                    break;
+                case 'K':
+                    whiteKingCount += 1;
+                    break;
+                case 'p':
+                    if (r == 0 || r == 7) {
+                        return {isValid: false, msg: "pawn can not exist on last rank or first rank."};
+                    }
+                case 'P':
+                    if (r == 0 || r == 7) {
+                        return {isValid: false, msg: "pawn can not exist on last rank or first rank."};
+                    }
+                case 'r':
+                case 'R':
+                case 'n':
+                case 'N':
+                case 'b':
+                case 'B':
+                case 'q':
+                case 'Q':
+            }
+        }
+    }
+}
